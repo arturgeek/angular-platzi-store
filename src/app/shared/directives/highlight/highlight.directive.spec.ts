b508614dc6c42609514723858f145649dfbf03da
LAPTOP-L6FFS2IF\Asus <Andres Morales>
2021-10-01 13:11:22 -0500

    shared core and home modules

--- src/app/shared/directives/highlight/highlight.directive.spec.ts
+++ src/app/shared/directives/highlight/highlight.directive.spec.ts
@@ -1,8 +1,10 @@
+import { ElementRef } from '@angular/core';
 import { HighlightDirective } from './highlight.directive';
 
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    let element:ElementRef = {} as ElementRef;
+    const directive = new HighlightDirective( element );
     expect(directive).toBeTruthy();
   });
 });
