5e924a695ef6113bde1869c0191e5c18831e8702
LAPTOP-L6FFS2IF\Asus <Andres Morales>
2021-10-04 18:10:13 -0500

    add material design to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
-import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LayoutComponent } from './layout/layout.component';
 
@@ -29,11 +28,11 @@ const routes: Routes = [
         path: "contact",
         loadChildren: () => import('./contact/contact.module').then( m => m.ContactModule )
       },
+      {
+        path: "demo",
+        loadChildren: () => import('./demo/demo.module').then( m => m.DemoModule )
+      }
     ]
-  },
-  {
-    path: "demo",
-    component: DemoComponent
   },
   {
     path: "**",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
 
-import { DemoComponent } from './demo/demo.component';
-
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { LayoutComponent } from './layout/layout.component';
 
 import { SharedModule } from './shared/shared.module'
-import { CoreModule } from './core/core.module'
+import { CoreModule } from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    DemoComponent,
     PageNotFoundComponent,
     LayoutComponent
   ],
@@ -27,7 +25,8 @@ import { CoreModule } from './core/core.module'
     AppRoutingModule,
     FormsModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
